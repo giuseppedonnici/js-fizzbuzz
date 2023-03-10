fc1c82da5076d62903b9e50cd73d8dd90bb15621
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-03-10 15:48:26 +0100

    Add: bonus 1 and bonus 2 - manipulating DOM

--- js/script.js
+++ js/script.js
@@ -3,23 +3,40 @@ const fizz = "Fizz";
 const buzz = "Buzz";
 const fizzBuzz = "FizzBuzz";
 
+// Creo una variabile per prendere l'elemento .grid
+const gridElem = document.querySelector(".grid");
+// Creo due variabili di appoggio che che valorizzerò in seguito
+let result;
+let bgColor;
+
 
 // Creo i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // Creo variabili di blocco per frammentare le condizioni
     const isDivisibleFor3 = (i % 3 === 0);
     const isDivisibleFor5 = (i % 5 ===0);
-    // Inizio quando entrambe sono verificate
+    // Inizio quando entrambe sono verificate e conservo l'informazione nella variabile di appoggio
     if (isDivisibleFor3 && isDivisibleFor5) {
         console.log(i, fizzBuzz);
-        // Altrimenti stampo i divisibili per 3
+        result = fizzBuzz;
+        bgColor = "yellow";
+        // Altrimenti stampo i divisibili per 3 e conservo l'informazione nella variabile di appoggio
     } else if (isDivisibleFor3) {
         console.log(i, fizz);
-        //Altrimenti stampo i divisibili per 5
+        result = fizz;
+        bgColor = "red";
+        //Altrimenti stampo i divisibili per 5 e conservo l'informazione nella variabile di appoggio
     } else if (isDivisibleFor5) {
         console.log(i, buzz);
-        //Tutti i restanti che non soddisfano le condizioni precedenti li stampo così come sono
+        result = buzz;
+        bgColor = "blue";
+        //Tutti i restanti che non soddisfano le condizioni precedenti li stampo così come sono e conservo l'informazione nella variabile di appoggio
     } else {
         console.log(i);
+        result = i;
+        bgColor = "";
     }
+    // Creo, per ogni ciclo, un div con la classe .box e rispettivo colore all'interno del container .grid
+    gridElem.innerHTML += `<div class="box ${bgColor}">${result}</div>`;
+
 }
