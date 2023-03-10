a1940af8c8f71e067b309d3e46e9df0724a92181
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-03-10 14:42:23 +0100

    Add: structure and style to html

--- js/script.js
+++ js/script.js
@@ -6,14 +6,19 @@ const fizzBuzz = "FizzBuzz";
 
 // Creo i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
+    // Creo variabili di blocco per frammentare le condizioni
     const isDivisibleFor3 = (i % 3 === 0);
     const isDivisibleFor5 = (i % 5 ===0);
+    // Inizio quando entrambe sono verificate
     if (isDivisibleFor3 && isDivisibleFor5) {
         console.log(i, fizzBuzz);
+        // Altrimenti stampo i divisibili per 3
     } else if (isDivisibleFor3) {
         console.log(i, fizz);
+        //Altrimenti stampo i divisibili per 5
     } else if (isDivisibleFor5) {
         console.log(i, buzz);
+        //Tutti i restanti che non soddisfano le condizioni precedenti li stampo così come sono
     } else {
         console.log(i);
     }
